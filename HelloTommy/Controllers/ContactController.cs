f7492d58657b627670d6d850c873d7fb79d0aa8d
Gabriel Enström <59197583+Genstrom@users.noreply.github.com>
2021-03-09 04:04:44 +0100

    Update ContactController.cs

--- HelloTommy/Controllers/ContactController.cs
+++ HelloTommy/Controllers/ContactController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using HelloTommy.Models;
 using hiTommy.Data.Services;
 using hiTommy.Data.ViewModels;
 
@@ -11,15 +14,19 @@ namespace HelloTommy.Controllers
 {
     public class ContactController : Controller
     {
+       
         public ShoeServices _shoesService;
         public BrandServices _brandServices;
 
-        public ContactController(ShoeServices shoeServices, BrandServices brandServices)
+        public ContactController(BrandServices brandServices, ShoeServices shoesService)
         {
             _brandServices = brandServices;
-            _shoesService = shoeServices;
+            _shoesService = shoesService;
         }
+
+
         [Route("{contact}")]
+       [HttpGet()]
         public IActionResult Index()
         {
             var allShoesVm = new ShoeListViewModel()
@@ -27,12 +34,65 @@ namespace HelloTommy.Controllers
                 Shoes = _shoesService.GetAllShoes()
             };
             var allBrandsVM = _brandServices.GetAllBrands();
+            var formModel = new ContaFormModel();
 
-            dynamic mymodel = new ExpandoObject();
-            mymodel.AllShoes = allShoesVm.Shoes;
-            mymodel.Brand = allBrandsVM;
+            dynamic myModel = new ExpandoObject();
+             
+            myModel.AllShoes = allShoesVm.Shoes;
+            myModel.Brand = allBrandsVM;
+            myModel.Contact = formModel;
 
-            return View(mymodel);
+
+            return View(myModel);
         }
+        
+        [HttpPost]
+       /* public IActionResult Index(string firstName)
+        {
+            return Content($"Hello {firstName}");
+        }*/
+        [HttpPost]  
+        public ActionResult Index( string name, string email, string message, string subject){  
+           var allShoesVm = new ShoeListViewModel()
+           {
+               Shoes = _shoesService.GetAllShoes()
+           };
+           var allBrandsVM = _brandServices.GetAllBrands();
+           var formModel = new ContaFormModel();
+
+           dynamic myModel = new ExpandoObject();
+             
+           myModel.AllShoes = allShoesVm.Shoes;
+           myModel.Brand = allBrandsVM;
+           myModel.Contact = formModel;
+           
+           try {  
+                if (ModelState.IsValid) {  
+                    var senderEmail = new MailAddress("hellotommyshoe@gmail.com","HelloTommyShoes");  
+                    var receiverEmail = new MailAddress("hellotommyshoe@gmail.com","Receiver");  
+                    var password = "ITHS2020!";
+                    var sub = subject;
+                    var body = $"From Name: {name} Email:{email} \n{message}";  
+                    var smtp = new SmtpClient {  
+                        Host = "smtp.gmail.com",  
+                        Port = 587,  
+                        EnableSsl = true,  
+                        DeliveryMethod = SmtpDeliveryMethod.Network,  
+                        UseDefaultCredentials = false,  
+                        Credentials = new NetworkCredential(senderEmail.Address, password)  
+                    };  
+                    using(var mess = new MailMessage(senderEmail, receiverEmail) {  
+                        Subject = sub,  
+                        Body = body,
+                    }) {  
+                        smtp.Send(mess);  
+                    }  
+                    return View(myModel);  
+                }  
+            } catch (Exception) {  
+                ViewBag.Error = "Some Error";  
+            }  
+            return View(myModel);  
+        }  
     }
 }
