1b649bbf063e0bb5c693cbc939ce4e2bc6178faa
Gabriel Enström <gabriel.enstrom@gmail.com>
2021-03-09 09:19:48 +0100

    Fixed ProductPage

--- HelloTommy/Controllers/ProductController.cs
+++ HelloTommy/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using hiTommy.Data.Services;
+﻿using System.Dynamic;
+using hiTommy.Data.Services;
+using hiTommy.Data.ViewModels;
+using hiTommy.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloTommy.Controllers
@@ -7,19 +10,29 @@ namespace HelloTommy.Controllers
     [Route("product")]
     public class ProductController : Controller
     {
+        private readonly BrandServices _brandServices;
         private readonly ShoeServices _shoesService;
 
-        public ProductController(ShoeServices shoeServices)
+        public ProductController(ShoeServices shoeServices, BrandServices brandServices)
         {
             _shoesService = shoeServices;
+            _brandServices = brandServices;
         }
 
         [Route("{productId:int}")]
         public IActionResult Index(int productId)
         {
-            var shoe = _shoesService.GetShoeById(productId);
+            
 
-            return View(shoe);
+               var shoe = _shoesService.GetShoeById(productId);
+      
+            var allBrandsVm = _brandServices.GetAllBrands();
+
+            dynamic mymodel = new ExpandoObject();
+            mymodel.Shoe = shoe;
+            mymodel.Brand = allBrandsVm;
+
+            return View(mymodel);
         }
     }
 }
