cb50dccf5d9e153ba96acec747623f5366451e37
Colin Farkas <Colin.Farkas@iths.se>
2021-03-11 22:09:27 +0100

    Adding checkout-page (html, css & Javascript)

---
+++ HelloTommy/Controllers/CheckoutController.cs
@@ -0,0 +1,43 @@
+﻿using hiTommy.Data.Services;
+using hiTommy.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloTommy.Controllers
+{
+    [Route("Checkout")]
+    public class CheckoutController : Controller
+    {
+        public BrandServices _brandServices;
+
+        public ShoeServices _shoesService;
+
+        public CheckoutController(BrandServices brandServices, ShoeServices shoesService)
+        {
+            _brandServices = brandServices;
+            _shoesService = shoesService;
+        }
+        
+        public IActionResult Checkout()
+        {
+            var allShoesVm = new ShoeListViewModel
+            {
+                Shoes = _shoesService.GetAllShoes()
+            };
+            var allBrandsVM = _brandServices.GetAllBrands();
+
+            dynamic myModel = new ExpandoObject();
+
+            myModel.Shoes = allShoesVm.Shoes;
+            myModel.Brand = allBrandsVM;
+
+
+            return View(myModel);
+        }
+
+    }
+}
